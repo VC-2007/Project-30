070c2101a4c46e970577708273443137831e4c5b
VC-2007 <77193469+VC-2007@users.noreply.github.com>
2021-05-30 17:09:43 +0530

    Add files via upload

--- Block.js
+++ Block.js
@@ -8,19 +8,20 @@ class Block{
       this.image = loadImage("block.png")
       this.width = width;
       this.height = height;
-      this.visibility = this.visibility
+      this.visibility = 255
       World.add(world, this.body);
     }
     display(){
       push()
+      
       if(this.body.speed <3){
       var pos= this.body.position;
       rectMode(CENTER);
       rect(pos.x,pos.y,this.width, this.height);
       }
       else{
-        World.remove(world,this.body)        
-        this.body.visibility = this.body.visibility - 5
+        World.remove(world,this.body)  
+        this.visibility = this.visibility - 5
         tint(225,this.visibility);
         image(this.image,this.body.position.x,this.body.position.y,30,40)
         pop()
